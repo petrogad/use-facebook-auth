11725a0c95cb185ec55806aeadeaee6c9abd84d0
Pete Carr <petrogad@gmail.com>
2020-12-02 08:40:46 -0600

    linting preferences

--- src/index.tsx
+++ src/index.tsx
@@ -2,112 +2,112 @@ import React, { ReactNode, useContext, useEffect, useState } from 'react';
 
 import { initFacebookSdk, login, logout } from './utils/facebook.client';
 import {
-  AuthResponse,
-  LoginStatus,
-  StatusResponse,
+	AuthResponse,
+	LoginStatus,
+	StatusResponse,
 } from './utils/facebook.window';
 
 interface FacebookContextOptions {
-  authenticatedState: AuthenticatedStatus;
-  authenticatedSession?: {};
-  isAuthenticated: boolean;
-  loading: boolean;
-  user: {};
-  logout(): void;
-  login(): void;
+	authenticatedState: AuthenticatedStatus;
+	authenticatedSession?: {};
+	isAuthenticated: boolean;
+	loading: boolean;
+	user: {};
+	logout(): void;
+	login(): void;
 }
 const DEFAULT_REDIRECT_CALLBACK = () =>
-  window.history.replaceState({}, document.title, window.location.pathname);
+	window.history.replaceState({}, document.title, window.location.pathname);
 
 enum AuthenticatedStatus {
-  LOADING = 'loading',
-  ERROR = 'error',
-  AUTHENTICATED = 'authenticated',
-  UNAUTHENTICATED = 'unauthenticated',
+	LOADING = 'loading',
+	ERROR = 'error',
+	AUTHENTICATED = 'authenticated',
+	UNAUTHENTICATED = 'unauthenticated',
 }
 
 export const FacebookContext = React.createContext<FacebookContextOptions>({
-  authenticatedState: AuthenticatedStatus.LOADING,
-  isAuthenticated: false,
-  loading: true,
-  user: {},
-  logout: () => {},
-  login: () => {},
+	authenticatedState: AuthenticatedStatus.LOADING,
+	isAuthenticated: false,
+	loading: true,
+	user: {},
+	logout: () => {},
+	login: () => {},
 });
 
 interface FacebookProviderParams {
-  children: ReactNode;
-  onRedirectCallback(state?: string): void;
-  options: {
-    appId: string;
-  };
+	children: ReactNode;
+	onRedirectCallback(state?: string): void;
+	options: {
+		appId: string;
+	};
 }
 
 export const useFacebook = () => useContext(FacebookContext);
 
 interface AuthStateDef {
-  authStatus: AuthenticatedStatus;
-  authResponse?: AuthResponse;
-  authRequestStatus?: LoginStatus;
-  user?: {};
+	authStatus: AuthenticatedStatus;
+	authResponse?: AuthResponse;
+	authRequestStatus?: LoginStatus;
+	user?: {};
 }
 
 export const FacebookProvider = ({
-  children,
-  onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
-  options,
+	children,
+	onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
+	options,
 }: FacebookProviderParams) => {
-  const [authState, setAuthState] = useState<AuthStateDef>({
-    authStatus: AuthenticatedStatus.LOADING,
-  });
+	const [authState, setAuthState] = useState<AuthStateDef>({
+		authStatus: AuthenticatedStatus.LOADING,
+	});
 
-  const handleSetAuthState = (response?: StatusResponse) => {
-    const authToken =
-      response && response.status === 'connected'
-        ? response.authResponse
-        : undefined;
+	const handleSetAuthState = (response?: StatusResponse) => {
+		const authToken =
+			response && response.status === 'connected'
+				? response.authResponse
+				: undefined;
 
-    setAuthState({
-      authStatus: authToken
-        ? AuthenticatedStatus.AUTHENTICATED
-        : AuthenticatedStatus.UNAUTHENTICATED,
-      authRequestStatus: response?.status,
-      authResponse: authToken,
-    });
-  };
+		setAuthState({
+			authStatus: authToken
+				? AuthenticatedStatus.AUTHENTICATED
+				: AuthenticatedStatus.UNAUTHENTICATED,
+			authRequestStatus: response?.status,
+			authResponse: authToken,
+		});
+	};
 
-  useEffect(() => {
-    const initFacebook = async () => {
-      const facebookFromHook = await initFacebookSdk({
-        appId: options.appId,
-      });
+	useEffect(() => {
+		const initFacebook = async () => {
+			const facebookFromHook = await initFacebookSdk({
+				appId: options.appId,
+			});
 
-      handleSetAuthState(facebookFromHook);
-    };
+			handleSetAuthState(facebookFromHook);
+		};
 
-    initFacebook();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+		initFacebook();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  return (
-    <FacebookContext.Provider
-      value={{
-        authenticatedState: authState.authStatus,
-        isAuthenticated:
-          authState.authStatus === AuthenticatedStatus.AUTHENTICATED,
-        loading: authState.authStatus === AuthenticatedStatus.LOADING,
-        user: {},
-        logout: async () => {
-          const response = await logout();
-          handleSetAuthState(response);
-        },
-        login: async () => {
-          const response = await login();
-          handleSetAuthState(response);
-        },
-      }}
-    >
-      {children}
-    </FacebookContext.Provider>
-  );
+	return (
+		<FacebookContext.Provider
+			value={{
+				authenticatedState: authState.authStatus,
+				isAuthenticated:
+					authState.authStatus === AuthenticatedStatus.AUTHENTICATED,
+				loading: authState.authStatus === AuthenticatedStatus.LOADING,
+				user: {},
+				logout: async () => {
+					const response = await logout();
+					handleSetAuthState(response);
+				},
+				login: async () => {
+					const response = await login();
+					handleSetAuthState(response);
+				},
+			}}
+		>
+			{children}
+		</FacebookContext.Provider>
+	);
 };
